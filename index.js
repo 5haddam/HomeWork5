711e614cb0d78f4059fdb69c8a3ad2eef4f2349f
5haddam <131292268+5haddam@users.noreply.github.com>
2023-04-26 23:01:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+firstNumber = +prompt('Enter first number');
+secondNumber = +prompt('Enter second number');
+thirdNumber = +prompt('Enter third number');
+
+sumOfNumbers = firstNumber + secondNumber + thirdNumber;
+averageValue = sumOfNumbers/3;
+
+alert(`Average value of this numbers is ${averageValue}`);
